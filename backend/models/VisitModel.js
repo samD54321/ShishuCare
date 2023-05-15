1b5249a1ec58c6874d49801bde4cd80c8bdca7b9
sital999 <sitalnagarkoti123@gmail.com>
2023-05-15 17:59:11 +0545

    add patient and diagnose as ref

--- backend/models/VisitModel.js
+++ backend/models/VisitModel.js
@@ -1,15 +1,19 @@
 const mongoose=require('mongoose');
 
-const VisitSchema= mongoose.Schema({
-    DOV:{type:Date,default: Date.now,required:true},
-    height:{type:Number,required:true},
-    weight:{type:Number,required:true},
-    dehydration:{type:Boolean,required:true},
-    skinColor:{type:String,required:true},
-    note:{type:String,required:true,maxLength:1000},
-    patient:{type:mongoose.Schema.Types.ObjectId,ref:'Patient',required:true},
-    CHW:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"CHW"}
-})
+const VisitSchema = mongoose.Schema({
+  DOV: { type: Date, default: Date.now, required: true },
+  height: { type: Number, required: true },
+  weight: { type: Number, required: true },
+  temperature: { type: Number, required: true },
+  skinColor: { type: String, required: true },
+  note: { type: String, required: true, maxLength: 1000 },
+  breathRate: { type: String, required: true },
+  drinkMilk: { type: Boolean, required: true },
+  dehydration: { type: Boolean, required: true },
+  patient:{type:mongoose.SchemaTypes.ObjectId,ref:"Person"},
+  diagnose:{type:Boolean, default: false}
+  // CHW:{type:mongoose.Schema.Types.ObjectId,required:true,ref:"CHW"}
+});
 
 const VisitModel= mongoose.model('Visit',VisitSchema)
 
