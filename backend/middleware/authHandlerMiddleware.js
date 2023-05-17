744128d2dbf4df4ca553d030622541bb8861217a
sital999 <sitalnagarkoti123@gmail.com>
2023-05-18 00:05:03 +0545

    create authentication with jwt

---
+++ backend/middleware/authHandlerMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+const asyncHandler = require("express-async-handler");
+
+const protectedRoute = asyncHandler(async (req, res, next) => {
+  let token = "";
+  if (req.headers && req.headers.authorization) {
+    token = req.headers.authorization.split(" ")[1];
+    try {
+      const data = jwt.verify(token, process.env.SECRET_KEY);
+      req.ID = data.id;
+      if (data.role === "CHW") {
+        req.CHW = data.role;
+      } else {
+        req.DOCTOR = data.role;
+      }
+      next();
+    } catch (error) {
+      throw new Error("Not Authorized, Invalid Token");
+    }
+  } else {
+    throw new Error("No authorization token. Please provide it.");
+  }
+});
+
+module.exports = { protectedRoute };
