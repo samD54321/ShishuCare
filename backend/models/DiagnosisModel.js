6b5341b196555d7b98b90a9bf964d4bbe8661e60
sital999 <sitalnagarkoti123@gmail.com>
2023-05-18 16:20:58 +0545

    add field for data based on role in model

--- backend/models/DiagnosisModel.js
+++ backend/models/DiagnosisModel.js
@@ -4,7 +4,7 @@ const DiagnoseSchema = mongoose.Schema({
     conclusion:{type: 'string',required:[true,'Please provide conclusion']},
     severity:{type: String,required: true,maxLength:20},
     prescriptions:{type: String,required: [true,'Please provide a prescription']},
-    // doctor:[{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Doctor'}],
+    doctor:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Doctor'},
     visit:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Visit'},
     patient:{type:mongoose.Schema.Types.ObjectId,required:true,ref:'Patient'},
 })  
