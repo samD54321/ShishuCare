b81a0ac2622cb70ba228e727aa3fb12d1786a1c7
sital999 <sitalnagarkoti123@gmail.com>
2023-05-18 16:19:24 +0545

    add RBAC in controlllers

--- backend/controller/diagnosisController.js
+++ backend/controller/diagnosisController.js
@@ -1,17 +1,70 @@
-const asyncHandler = require('express-async-handler');
-const {Diagnosis,Visit}= require('../models')
-const mongoose = require('mongoose')
+const asyncHandler = require("express-async-handler");
+const { Diagnosis, Visit } = require("../models");
+const { isPermissionGranted } = require("../config/accessControl");
+const actions = require("../constants/action_RBAC");
 
-const getDiagnosis= asyncHandler(async(Req,res)=>{
-    const diagnosis= await Diagnosis.find().populate('patient').populate('visit')
-    res.status(200).json({data:diagnosis})
-})
+const getDiagnosis = asyncHandler(async (req, res) => {
+  const permission = isPermissionGranted(
+    req.role,
+    "diagnosis",
+    actions.READ_ANY
+  );
+  if (!permission) {
+    res.statusCode = 403;
+    throw new Error("You are forbidden to access this");
+  } else {
+    const diagnosis = await Diagnosis.find()
+      .populate("patient")
+      .populate("visit")
+      .populate("doctor");
+    res.status(200).json({ data: diagnosis });
+  }
+});
 
-const createDiagnosis= asyncHandler(async(req,res)=>{
-  const visit = await Visit.findById(req.params.visitId);
-  const diagnosis= await Diagnosis.create({...req.body,visit:req.params.visitId,patient:visit.patient})
-  await Visit.updateOne({_id:req.params.visitId},{diagnosis:diagnosis._id})
-  res.status(201).json({data:diagnosis})
-})
+const createDiagnosis = asyncHandler(async (req, res) => {
+  const permission = isPermissionGranted(
+    req.role,
+    "diagnosis",
+    actions.CREATE_ANY
+  );
+  if (!permission) {
+    res.statusCode = 403;
+    throw new Error("You are forbidden to access this");
+  } else {
+    const visit = await Visit.findById(req.params.visitId);
+    const diagnosis = await Diagnosis.create({
+      ...req.body,
+      visit: req.params.visitId,
+      patient: visit.patient,
+      doctor: req.ID,
+    });
+    await Visit.updateOne(
+      { _id: req.params.visitId },
+      { diagnosis: diagnosis._id }
+    );
+    res.status(201).json({ data: diagnosis });
+  }
+});
 
-module.exports = { createDiagnosis, getDiagnosis };
+const updateDiagnosis = asyncHandler(async (req, res) => {
+  const permission = isPermissionGranted(
+    req.role,
+    "diagnosis",
+    actions.UPDATE_OWN
+  );
+
+  if (!permission) {
+    res.statusCode = 403;
+    throw new Error("You are forbidden to access this");
+  } else {
+    const diagnosis = await Diagnosis.findOne({ visit: req.params.visitId });
+    if (!diagnosis) {
+      res.statusCode = 400;
+      throw new Error("No diagnosis found with such id");
+    }
+    await Diagnosis.updateOne({ visit: req.params.visitId }, req.body);
+    return res.status(200).json({ data: { msg: "sucessfully updated" } });
+  }
+});
+
+module.exports = { createDiagnosis, getDiagnosis, updateDiagnosis };
--- backend/controller/visitController.js
+++ backend/controller/visitController.js
@@ -1,28 +1,51 @@
-const asyncHandler= require('express-async-handler');
-const {Patient,Visit} = require('../models')
-const mongoose = require('mongoose')
+const asyncHandler = require("express-async-handler");
+const { Patient, Visit } = require("../models");
+const mongoose = require("mongoose");
+const { isPermissionGranted } = require("../config/accessControl");
+const actions = require("../constants/action_RBAC");
 
-const allVisit=asyncHandler(async(req,res)=>{
-    const visit= await Visit.find()
-    res.json(visit)
-})
+const allVisit = asyncHandler(async (req, res) => {
+  const visit = await Visit.find();
+  res.json(visit);
+});
 
-const getVisit= asyncHandler(async(req,res)=>{
-    const patientVisit= await Visit.findById(req.params.visitId).populate('diagnosis')
-    if (!patientVisit) throw new Error(`No such visit id : ${req.params.visitId} exists. `)
-    res.status(200).json({data: patientVisit})
-})
+const getVisit = asyncHandler(async (req, res) => {
+  const permission = isPermissionGranted(req.role, "visit", actions.READ_ANY);
+  if (!permission) {
+    res.statusCode = 403;
+    throw new Error("You are forbidden to access this");
+  } else {
+    const patientVisit = await Visit.findById(req.params.visitId).populate(
+      "diagnosis"
+    );
+    if (!patientVisit)
+      throw new Error(`No such visit id : ${req.params.visitId} exists. `);
+    res.status(200).json({ data: patientVisit });
+  }
+});
 
-const registerVisit= asyncHandler(async(req,res)=>{
-    
-    const patientVisit = await Visit.create({...req.body,patient:req.params.patientId})
-    const patient= await Patient.findById(req.params.patientId)
-    if (!patient) throw new Error(`Such Patient does not exist`)
-    const patientID=new mongoose.Types.ObjectId(patientVisit._id)
-    patient.visits.push(patientID)
-    patient.save()
+const registerVisit = asyncHandler(async (req, res) => {
+  const permission = isPermissionGranted(req.role, "visit", actions.CREATE_ANY);
+  if (!permission) {
+    res.statusCode = 403;
+    throw new Error("You are forbidden to access this");
+  } else {
+    const patientId = new mongoose.Types.ObjectId(req.params.patientId);
+    const patientVisit = await Visit.create({
+      ...req.body,
+      patient: patientId,
+      CHW: new mongoose.Types.ObjectId(req.ID),
+    });
+    const patient = await Patient.findById(req.params.patientId);
+    if (!patient) {
+      res.statusCode = 400;
+      throw new Error(`Such Patient does not exist`);
+    }
+    const patientID = new mongoose.Types.ObjectId(patientVisit._id);
+    patient.visits.push(patientID);
+    patient.save();
     res.status(201).json({ data: patientVisit });
-
-})
+  }
+});
 
 module.exports = { getVisit, registerVisit, allVisit };
