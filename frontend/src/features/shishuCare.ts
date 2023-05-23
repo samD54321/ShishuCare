02d855fd65878d9c80f2eb24f0ba50fa8a308a62
sital999 <sitalnagarkoti123@gmail.com>
2023-05-23 23:54:50 +0545

    change testSlice to shishuCareSlice

---
+++ frontend/src/features/shishuCare.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { LocalStorageItem } from '@auth/auth';
+import { ILocalStorageItem } from '@interfaces/index';
+
+const User = LocalStorageItem.getItem();
+
+interface IShishuCare {
+  isDoctorLogin: boolean;
+  isCHWLogin: boolean;
+  user: ILocalStorageItem | {};
+}
+
+const initialState: IShishuCare = {
+  isDoctorLogin: false,
+  isCHWLogin: false,
+  user: User ? User : {},
+};
+
+const shishuCareSlice = createSlice({
+  name: 'shishu_Care',
+  initialState,
+  reducers: {
+    logout: (state) => {
+      state.isDoctorLogin = false;
+      state.isCHWLogin = false;
+    },
+    loginDoctor: (state) => {
+      state.isDoctorLogin = true;
+      state.isCHWLogin = false;
+    },
+    loginCHW: (state) => {
+      state.isCHWLogin = true;
+      state.isDoctorLogin = false;
+    },
+  },
+});
+
+export const { logout, loginDoctor, loginCHW } = shishuCareSlice.actions;
+export default shishuCareSlice.reducer;
