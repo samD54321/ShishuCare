b6042129444fbe6ed7fb33db93eac2d648a83ae0
sital999 <sitalnagarkoti123@gmail.com>
2023-05-23 23:53:44 +0545

    make dashboard page

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import React from 'react';
+import { DoctorDashboard, CHWDashBoard } from '@components/dashboard';
+import { useAuth } from '@hooks/useAuth';
+import { useRouter } from 'next/navigation';
+
+const Dashboard = () => {
+  const router = useRouter();
+  const { authenticated, isDoctorLogin, isCHWLogin } = useAuth();
+  if (!authenticated) {
+    router.push('/login');
+  }
+  return <>{isDoctorLogin ? <DoctorDashboard /> : <CHWDashBoard />}</>;
+};
+
+export default Dashboard;
