e558784343eca1d59c9dceb4f5171d5f1fb0e483
sital999 <sitalnagarkoti123@gmail.com>
2023-05-24 16:06:22 +0545

    handle logout

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,61 +1,74 @@
 'use client';
 
-import { Container,Button ,Box} from '@mui/material';
+import { Container, Button, Box } from '@mui/material';
 import { Header } from '@components/Header';
 import { useDispatch } from 'react-redux';
 import { LocalStorageItem } from '@auth/auth';
 import { loginDoctor, loginCHW } from '@features/shishuCare';
+import { useRouter } from 'next/navigation';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const router = useRouter();
 
-   const user = LocalStorageItem.getItem().role;
-   if (user === 'Doctor') {
-     dispatch(loginDoctor());
-   } else {
-     dispatch(loginCHW());
-   }
+  const user = LocalStorageItem.getItem().role;
+  if (user === 'Doctor') {
+    dispatch(loginDoctor());
+  } else {
+    dispatch(loginCHW());
+  }
+  const handleClick = () => {
+    LocalStorageItem.clearItem();
+    router.push('/login');
+  };
   return (
-    <html lang="en">
-      <title>ShishuCare</title>
-      <body suppressHydrationWarning={true} style={{ padding: 0, margin: 0 }}>
-        <Container
-          maxWidth="xl"
+    // <html lang="en">
+    //   <title>ShishuCare</title>
+    // <body suppressHydrationWarning={true} style={{ padding: 0, margin: 0 }}>
+    <Container maxWidth="xl">
+      <Container
+        maxWidth="xl"
+        sx={{
+          bgcolor: 'white',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexDirection: 'row',
+          margin: 0,
+          // width:'250rem',
+          h4: {
+            margin: '1rem',
+            fontSize: ['1.5rem', '2.5rem'],
+            paddingInlineStart: '0.5rem',
+            color: '#234AAF',
+          },
+        }}
+      >
+        <Box
           sx={{
             bgcolor: 'white',
+            borderRadius: '100%',
+            padding: '1rem',
             display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
             flexDirection: 'row',
-            margin: 0,
-            // width:'250rem',
-            h4: {
-              margin: '1rem',
-              fontSize: ['1.5rem', '2.5rem'],
-              paddingInlineStart: '0.5rem',
-              color: '#234AAF',
-            },
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
         >
-          <Box
-            sx={{
-              bgcolor: 'white',
-              borderRadius: '100%',
-              padding: '1rem',
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Header />
-          </Box>
-          <Button sx={{ height: '50%', bgcolor: '#234AAF', borderRadius:"10%" }} variant="contained">
-            Logout
-          </Button>
-        </Container>
-        {children}
-      </body>
-    </html>
+          <Header />
+        </Box>
+        <Button
+          onClick={handleClick}
+          sx={{ height: '50%', bgcolor: '#234AAF', borderRadius: '10%', marginRight: 6 }}
+          variant="contained"
+        >
+          Logout
+        </Button>
+      </Container>
+      {children}
+    </Container>
+
+    /* </body>
+    </html> */
   );
 }
