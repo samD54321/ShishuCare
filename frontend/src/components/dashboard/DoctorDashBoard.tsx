931cfcc612cb23663efa87ca94ab101a24fc76f7
sital999 <sitalnagarkoti123@gmail.com>
2023-05-25 16:04:34 +0545

    use link in dashboard for another page

--- frontend/src/components/dashboard/DoctorDashBoard.tsx
+++ frontend/src/components/dashboard/DoctorDashBoard.tsx
@@ -3,10 +3,10 @@
 import { Card, Container, Paper, Typography, Button } from '@mui/material';
 import React from 'react';
 import { useGetPatientsQuery } from '@features/patient/patientApi';
+import Link from 'next/link';
 
 const DoctorDashBoard = () => {
   const { data, isLoading, error } = useGetPatientsQuery('');
-  console.log(data);
   const RecurringPatients = data?.RecurringPatients;
   const NewPatients = data?.NewPatients;
   if (isLoading) {
@@ -53,103 +53,106 @@ const NewCases = ({ datas }: { datas: any }) => {
     <Container maxWidth="xl">
       {datas.map((data: any, index: number) => {
         return (
-          <Paper
-            elevation={24}
-            key={index}
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              color: '#234AAF',
-              width: '100%',
-              marginBlock: 4,
-              padding: 1,
-              borderRadius: '10px',
-              alignItems: 'center',
-              boxShadow: '2px 3px 5px 2px black',
-              div: {
-                paddingInlineStart: 4,
-              },
-            }}
-          >
-            <div>
-              <Typography variant="h6">Case #{index + 1}</Typography>
-            </div>
-            <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography variant="h6">Name : {data.name}</Typography>
-              <Typography variant="h6">
-                Major Symptom : {data.visits[0].note.split(',')[0].toUpperCase()}
-              </Typography>
-            </Container>
+          <Link href={`/dashboard/${data._id}`} style={{ textDecoration: 'none' }}>
             <Paper
               elevation={24}
+              key={index}
               sx={{
-                width: '10%',
-                bgcolor: 'yellow',
-                borderRadius: '16px',
-                paddingBlock: 1,
-                marginInlineEnd: 2,
+                display: 'flex',
+                flexDirection: 'row',
+                color: '#234AAF',
+                width: '100%',
+                marginBlock: 4,
+                padding: 1,
+                borderRadius: '10px',
+                alignItems: 'center',
+                boxShadow: '2px 3px 5px 2px black',
+                div: {
+                  paddingInlineStart: 4,
+                },
               }}
             >
-              <Typography variant="body1" align="left">
-                Medium
-              </Typography>
+              <div>
+                <Typography variant="h6">Case #{index + 1}</Typography>
+              </div>
+              <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="h6">Name : {data.name}</Typography>
+                <Typography variant="h6">
+                  Major Symptom : {data.visits[0].note.split(',')[0].toUpperCase()}
+                </Typography>
+              </Container>
+              <Paper
+                elevation={24}
+                sx={{
+                  width: '10%',
+                  bgcolor: 'yellow',
+                  borderRadius: '16px',
+                  paddingBlock: 1,
+                  marginInlineEnd: 2,
+                }}
+              >
+                <Typography variant="body1" align="left">
+                  Medium
+                </Typography>
+              </Paper>
             </Paper>
-          </Paper>
+          </Link>
         );
       })}
     </Container>
   );
 };
 
-
 const RecurringCases = ({ datas }: { datas: any }) => {
-  console.log(datas[0])
+  console.log(datas[0]);
   return (
     <Container maxWidth="xl">
       {datas.map((data: any, index: number) => {
         return (
-          <Paper
-            elevation={24}
-            key={index}
-            sx={{
-              display: 'flex',
-              flexDirection: 'row',
-              color: '#234AAF',
-              width: '100%',
-              marginBlock: 4,
-              padding: 1,
-              borderRadius: '10px',
-              alignItems: 'center',
-              boxShadow: '2px 3px 5px 2px black',
-              div: {
-                paddingInlineStart: 4,
-              },
-            }}
-          >
-            <div>
-              <Typography variant="h6">Case #{index + 1}</Typography>
-            </div>
-            <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-              <Typography variant="h6">Name : {data.name}</Typography>
-              <Typography variant="h6">
-                Major Symptom : {data.visits[0].note.split(',')[0].toUpperCase()}
-              </Typography>
-            </Container>
+          <Link href={`/dashboard/${data._id}`} style={{ textDecoration: 'none' }}>
             <Paper
               elevation={24}
+              key={index}
               sx={{
-                width: '10%',
-                bgcolor: 'yellow',
-                borderRadius: '16px',
-                paddingBlock: 1,
-                marginInlineEnd: 2,
+                display: 'flex',
+                flexDirection: 'row',
+                color: '#234AAF',
+                width: '100%',
+                marginBlock: 4,
+                padding: 1,
+                borderRadius: '10px',
+                alignItems: 'center',
+                boxShadow: '2px 3px 5px 2px black',
+                div: {
+                  paddingInlineStart: 4,
+                },
               }}
             >
-              <Typography variant="body1" align="left">
-                Medium
-              </Typography>
+              <div>
+                <Typography variant="h6">Case #{index + 1}</Typography>
+              </div>
+              <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Typography variant="h6">Name : {data.name}</Typography>
+                <Typography variant="h6">
+                  Major Symptom : {data.visits[0].note.split(',')[0].toUpperCase()}
+                </Typography>
+              </Container>
+              <Paper
+                elevation={24}
+                sx={{
+                  width: '10%',
+                  bgcolor: 'yellow',
+                  borderRadius: '16px',
+                  paddingBlock: 1,
+                  marginInlineEnd: 2,
+                }}
+              >
+                <Typography variant="body1" align="left">
+                  Medium
+                </Typography>
+              </Paper>
             </Paper>
-          </Paper>
+          </Link>
         );
       })}
     </Container>
