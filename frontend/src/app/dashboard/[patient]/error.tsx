ca8e75c3170f4fd4c14cd0167f4f82307fe582e5
sital999 <sitalnagarkoti123@gmail.com>
2023-05-30 11:53:35 +0545

    add error page on [Patient]

---
+++ frontend/src/app/dashboard/[patient]/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react'
+
+const error = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', color: 'red' }}>
+      <h1>This Page does not exist. Go back clicking logo.</h1>
+    </div>
+  );
+}
+
+export default error
