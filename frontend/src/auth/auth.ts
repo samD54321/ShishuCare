12e5ef40300c95692d3850fcc9aea3dcca5a734a
sital999 <sitalnagarkoti123@gmail.com>
2023-05-31 17:23:52 +0545

    change name of item in localStorage

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -2,11 +2,11 @@ import { ILocalStorageItem } from "@interfaces/index";
 
 export class LocalStorageItem {
   static getItem = (): ILocalStorageItem => {
-    return JSON.parse(localStorage.getItem('user') || '{}');
+    return JSON.parse(localStorage.getItem('User') || '{}');
   };
 
   static setItem = (user: ILocalStorageItem) => {
-    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('User', JSON.stringify(user));
   };
 
   static clearItem = () => {
